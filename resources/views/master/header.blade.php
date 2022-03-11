841c341ab5e82897a9f31ebc4f5523fb353c2ac7
tzelginianc <tzelginia78@gmail.com>
2022-03-11 08:38:25 +0700

    revisi view

---
+++ resources/views/master/header.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">
+              <i class="fa fa-line-chart"></i>
+              Digital Trend
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="about-us" class="nav-link smoothScroll">About-us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="product" class="nav-link smoothScroll">Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="home" class="nav-link smoothScroll">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="news" class="nav-link">news</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="program" class="nav-link">program</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact-us" class="nav-link contact">contact-us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
